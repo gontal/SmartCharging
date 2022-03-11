5f464b7d3ac9da16032743b35aa926ce0932b9fa
Любовь Гонца <lgonta.job@gmail.com>
2022-03-11 23:01:49 +0100

    changed unit tests to have dynamic data

--- Jedlix.UnitTests/EngineShould.cs
+++ Jedlix.UnitTests/EngineShould.cs
@@ -2,6 +2,7 @@ using Jedlix.Controller;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using Assert = NUnit.Framework.Assert;
 
@@ -40,21 +41,55 @@ namespace Jedlix.UnitTests
 
             engine = new GreedyEngine();
         }
+        private static IEnumerable<object[]> GetTestTariffs()
+        {
+            return new List<object[]>()
+            {
+                 new object[]{ new List<Tariff>() { new Tariff(new DateTime(2022, 3, 4, 18, 0, 0), new DateTime(2022, 3, 4, 23, 0, 0), 10) }, 
+                                                    new DateTime(2022, 3, 4, 20, 0, 0), 
+                                                    new DateTime(2022, 3, 4, 9, 0, 0) },
+
+                 new object[]{ new List<Tariff>() { new Tariff(new DateTime(2022, 3, 4, 9, 0, 0), new DateTime(2022, 3, 4, 12, 0, 0), 10),
+                                                    new Tariff(new DateTime(2022, 3, 4, 18, 0, 0), new DateTime(2022, 3, 4, 23, 0, 0), 10)}, 
+                                                    new DateTime(2022, 3, 4, 20, 0, 0), 
+                                                    new DateTime(2022, 3, 4, 9, 0, 0) }
+            };
+        }
 
         [TestMethod]
-        public void AddOneDayIfLeavingTimeIsLessThanStartingTime()
+        [DataRow(2022, 3, 4, 20, 0)]
+        [DataRow(2022, 3, 4, 20, 9)]
+        [DataRow(2022, 3, 4, 20, 19)]
+        public void AddOneDay_IfLeavingTimeIsLessThan_StartingTime(int year, int month, int day, int startingHour, int leavingHour)
         {
-            var result = engine.CalculateLeavingDate(data.StartingTime, data.UserSettings.LeavingTime);
+            var startingTime = new DateTime(year, month, day, startingHour, 0, 0);
+            var leavingTime = new DateTime(year, month, day, leavingHour, 0, 0);
+            var result = engine.CalculateLeavingDate(startingTime, leavingTime);
 
-            Assert.AreEqual(data.UserSettings.LeavingTime.AddDays(1), result);
+            Assert.AreEqual(leavingTime.AddDays(1), result);
+        }
+
+        [TestMethod]
+        [DataRow(2022, 3, 4, 20, 20)]
+        [DataRow(2022, 3, 4, 20, 21)]
+        [DataRow(2022, 3, 4, 20, 23)]
+        public void LeavingDate_ShouldBeTheSameAsStartingDate_If_LeavingTimeIsMoreOrEqual_StartingTime(int year, int month, int day, int startingHour, int leavingHour)
+        {
+            var startingTime = new DateTime(year, month, day, startingHour, 0, 0);
+            var leavingTime = new DateTime(year, month, day, leavingHour, 0, 0);
+            var result = engine.CalculateLeavingDate(startingTime, leavingTime);
+
+            Assert.AreEqual(startingTime.Date, result.Date);
         }
         [TestMethod]
-        public void GetsTarifsForTwoDays()
+        [DynamicData(nameof(GetTestTariffs), DynamicDataSourceType.Method)]
+        public void ShouldDuplicateTarrifsForTheNextDate(List<Tariff> tariffs, DateTime startingDate, DateTime leavingDate)
         {
-            var result = engine.GetAllTariffs(data.UserSettings.Tariffs, data.StartingTime, data.UserSettings.LeavingTime.AddDays(1));
+            var result = engine.GetAllTariffs(tariffs, startingDate, data.UserSettings.LeavingTime.AddDays(1));
 
-            Assert.AreEqual(result.Count, 4);
+            Assert.AreEqual(result.Count, tariffs.Count * 2);
         }
+
 
     }
 }
